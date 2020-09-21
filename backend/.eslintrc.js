c3709103917a36ae83d194069ceb21712c575c48
Tiri <thierrylaval@gmx.com>
2020-09-21 10:31:46 +0200

    Update .eslintrc.js

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,3 +1,5 @@
+//ESLint est un outil linting utility (linter) pour le JavaScript. ... Ainsi dans son fonctionnement, ESlint va comparer notre code source à un ensemble de règles qui ont été définit et il va nous aider à les respecter.
+
 module.exports = {
     "env": {
         "browser": true,
